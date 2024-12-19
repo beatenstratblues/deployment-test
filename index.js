70b42e92b8763534fe5d4fc990c7c8532b67920d
Jatin Singh <70967350+beatenstratblues@users.noreply.github.com>
2024-12-19 16:59:46 +0530

    final test commit

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ app.get('/health', (req, res) => {
   return res.status(200).send("ok");
 });
 
-
 app.get("/test", (req, res) => {
   res.json({
     status: "sucessfully started",
+  });
+});
+
+app.get("/purpose", (req, res) => {
+  res.json({
+    status: "this is a test for ci/cd pipeline and its successfull",
   });
 });
 
